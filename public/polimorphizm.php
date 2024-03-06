aa0bd67882e72028a53729ab60a084c5bee85496
Vadim Podolyan <vadim.podolyan@deonlinedrogist.nl>
2024-03-06 14:05:55 +0200

    polimorphizm lesson

---
+++ public/polimorphizm.php
@@ -0,0 +1,68 @@
+<?php
+
+interface Calculate
+{
+    public function getArea(array $sides): string;
+}
+
+class Figure implements Calculate
+{
+   
+    protected function run()
+    {
+        echo 'Parent' . PHP_EOL;
+        $sides = [];
+        $sides[] = 1000;
+        $sides[] = 1000;
+        $result = $this->getArea($sides);
+        echo 'Parent result: ' . $result . PHP_EOL;
+    }
+    public function getArea(array $sides): string
+    {
+        $result = $sides[0];
+        return (string) $result;
+    }
+}
+class Square extends Figure
+{
+    public function run()
+    {
+        parent::run();
+        $sides = [];
+        $sides[] = 10;
+        $figure = "square";
+        $result = $this->getArea($sides);
+        
+        echo 'Square result: ' . $result . PHP_EOL;        
+    }
+    public function getArea(array $sides): string
+    {
+        $result = $sides[0] * 2;
+        return $result;
+    }
+}
+class Rectangle extends Figure
+{
+    public function run()
+    {
+        parent::run();
+        $sides = [];
+        $sides[] = 10;
+        $sides[] = 16;
+        $figure = "rectangle";
+        $result = $this->getArea($sides);
+        echo 'Rectangle result: ' . $result . PHP_EOL;        
+    }
+    public function getArea(array $sides): string
+    {
+        $result = $sides[0] * $sides[1];
+        return (string) $result;
+    }
+}
+$square = new Square();
+$square->run();
+
+$rectangle = new Rectangle();
+$rectangle->run();
+
+
